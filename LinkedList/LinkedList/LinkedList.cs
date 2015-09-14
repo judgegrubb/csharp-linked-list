37b2013cc1ea8ba6ffa5f478acfe6a306dd48eb0
judgegrubb <elijah.grubb@utah.edu>
2015-09-14 15:21:10 -0600

    Created LinkedList class and a couple tests

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList<T>
+    {
+        private class Node<E>
+        {
+            private E Data;
+            private Node<E> Child;
+
+            public Node()
+            {
+                Data = default(E);
+                Child = null;
+            }
+
+            public Node(E d)
+            {
+                Data = d;
+                Child = null;
+            }
+
+            public void setChild(Node<E> c)
+            {
+                Child = c;
+            }
+
+            public void setData(E d)
+            {
+                Data = d;
+            }
+
+            public E getData()
+            {
+                return Data;
+            }
+
+            public Node<E> getChild()
+            {
+                return Child;
+            }
+        }
+
+        private Node<T> Head;
+        private Node<T> Tail;
+        private int Count;
+
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public void AddLast(T data)
+        {
+            if (Count == 0)
+            {
+                Head = new Node<T>(data);
+                Tail = Head;
+            }
+            else
+            {
+                Node<T> Temp = new Node<T>(data);
+                Tail.setChild(Temp);
+                Tail = Temp;
+            }
+
+            Count++;
+        }
+
+        public T GetLast()
+        {
+            return Tail.getData();
+        }
+
+        public void AddFirst(T data)
+        {
+            if (Count == 0)
+            {
+                Head = new Node<T>(data);
+                Tail = Head;
+            }
+            else
+            {
+                Node<T> Temp = new Node<T>(data);
+                Temp.setChild(Head);
+                Head = Temp;
+            }
+
+            Count++;
+        }
+
+        public T GetFirst()
+        {
+            return Head.getData();
+        }
+
+        public T PopFirst()
+        {
+            T returnData = Head.getData();
+            Head = Head.getChild();
+            Count--;
+            return returnData;
+        }
+
+        public T PopLast()
+        {
+            T returnData = Tail.getData();
+            Tail = FindSecondToLast();
+            Tail.setChild(null);
+            Count--;
+            return returnData;
+        }
+
+        public int getCount()
+        {
+            return Count;
+        }
+
+        public bool Contains(T data)
+        {
+            Node<T> current = Head;
+            while (current != null)
+            {
+                if (!current.getData().Equals(data))
+                {
+                    current = current.getChild();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private Node<T> FindSecondToLast()
+        {
+            Node<T> current = Head;
+            while (current.getChild() != null && current.getChild() != Tail)
+            {
+                current = current.getChild();
+            }
+
+            return current;
+        }
+    }
+}
---
+++ LinkedList/TestDataStructures/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
+
+namespace TestDataStructures
+{
+    [TestClass]
+    public class TestDS
+    {
+        [TestMethod]
+        public void TestLinkedListAddFirstAndGetFirst()
+        {
+            LinkedList<int> ll = new LinkedList<int>();
+            ll.AddFirst(5);
+            Assert.AreEqual(5, ll.GetFirst());
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            LinkedList<int> ll = new LinkedList<int>();
+            ll.AddFirst(5);
+            ll.AddLast(7);
+            ll.AddLast(9);
+            ll.AddLast(9);
+            ll.AddLast(9);
+            ll.AddLast(9);
+            ll.AddLast(9);
+            ll.AddLast(9);
+            ll.AddLast(9);
+            ll.AddLast(8);
+            ll.AddLast(9);
+            ll.AddLast(9);
+            ll.AddLast(9);
+            ll.AddLast(9);
+            Assert.AreEqual(true, ll.Contains(8));
+            Assert.AreEqual(false, ll.Contains(6));
+        }
+    }
+}
